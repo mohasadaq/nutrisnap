e36f0102d28c84f8ba661098b12ffa54a3fa632f
mohamed abdirahman <mohamed.abdirahman@paysii.com>
2025-06-16 09:19:45 +0000

    when I try to make npm run build :  Server Actions are not supported wi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export', // Removed to support Server Actions
   images: {
     unoptimized: true,
   },
